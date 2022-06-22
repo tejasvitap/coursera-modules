df9ca4ec8233a80ed21a1e601ed3a0d16c3b1923
tejasvitap <tejasvita.patil@gmail.com>
2022-06-22 12:56:04 +0530

    New Module4 updated

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,5 +1,5 @@
 
-
+/*
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 
@@ -13,5 +13,18 @@ for (var i = 0; i < names.length; i++) {
     } else {
       helloSpeaker.speak(names[i]);
     }
-}
+}*/
+
+(function(){
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for(var i =0; i<names.length;i++){
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if(firstLetter === 'j'){
+            byeSpeaker.speak(names[i]);
+        }
+        else{
+            helloSpeaker.speak(names[i]);        
+        }
+    }
+})();
 
